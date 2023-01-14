442579f47993cfa7a962ff9a397961b25556285b
Youssif Tawfik <youssif.tawfik96@gmail.com>
2023-01-14 13:44:34 +0200

    - Implement test cases over User service

--- src/main/java/com/pocket/wallet/services/user/UserService.java
+++ src/main/java/com/pocket/wallet/services/user/UserService.java
@@ -4,14 +4,10 @@ import com.pocket.wallet.entities.UserCategory;
 import com.pocket.wallet.entities.UserEntity;
 import com.pocket.wallet.exceptions.UserCategoryNotFound;
 import com.pocket.wallet.models.UserModel;
-import com.pocket.wallet.repositories.IWalletRepository;
 import com.pocket.wallet.repositories.UserRepository;
-import com.pocket.wallet.response.IBasicResponse;
-import com.pocket.wallet.response.UserResponse;
 import com.pocket.wallet.services.category.CategoryService;
 import com.pocket.wallet.utilities.AESCipher;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
---
+++ src/test/java/com/pocket/wallet/services/user/UserServiceTest.java
@@ -0,0 +1,90 @@
+package com.pocket.wallet.services.user;
+
+import com.pocket.wallet.entities.UserCategory;
+import com.pocket.wallet.entities.UserEntity;
+import com.pocket.wallet.exceptions.UserCategoryNotFound;
+import com.pocket.wallet.models.UserModel;
+import com.pocket.wallet.repositories.UserRepository;
+import com.pocket.wallet.services.category.CategoryService;
+import com.pocket.wallet.utilities.AESCipher;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private AESCipher aesCipher;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private CategoryService categoryService;
+    @Mock
+    private UserHelper userHelper;
+    @InjectMocks
+    private UserService userService;
+
+    private UserModel userModel;
+    private UserEntity userEntity;
+    private UserCategory userCategory;
+
+    @BeforeEach
+    private void init(){
+        userModel=UserModel.builder()
+                .firstName("Test")
+                .lastName("Test")
+                .email("test@gmail.com")
+                .phone("0152336652")
+                .password("Hello")
+                .category("VIP").build();
+
+        userEntity=new UserEntity();
+        userEntity.setFirstName("Test");
+        userEntity.setLastName("Test");
+        userEntity.setEmail("test@gmail.com");
+        userEntity.setPhone("0152336652");
+        userEntity.setPassword("Hello");
+
+        userCategory=UserCategory.builder()
+                .code("VIP")
+                .dailyDebitLimit(1000.0).dailyCreditLimit(2000.0).build();
+    }
+
+
+    @DisplayName("Service: Add User Test")
+    @Test
+    public void givenUserModel_whenAddUser_thenReturnSavedObject() throws Exception {
+        // given
+        BDDMockito.given(aesCipher.encrypt(userModel.getPassword())).willReturn("Password");
+        BDDMockito.given(userHelper.mapToEntity(userModel)).willReturn(userEntity);
+        BDDMockito.given(categoryService.getCategoryByCode(userModel.getCategory())).willReturn(userCategory);
+        // when
+        UserModel responseUserModel=userService.addUser(userModel);
+        // then
+        BDDMockito.verify(userRepository, Mockito.times(1)).save(userEntity);
+        Assertions.assertThat(responseUserModel).isNotNull();
+    }
+
+    @DisplayName("Service: Add User Test - Throw Exception")
+    @Test
+    public void givenUserModel_whenAddUser_thenThrowsCategoryNotFound() throws Exception {
+        // given
+        BDDMockito.given(aesCipher.encrypt(userModel.getPassword())).willReturn("Password");
+        BDDMockito.given(userHelper.mapToEntity(userModel)).willReturn(userEntity);
+        BDDMockito.given(categoryService.getCategoryByCode(userModel.getCategory())).willReturn(null);
+        // when
+        org.junit.jupiter.api.Assertions.assertThrows(UserCategoryNotFound.class, ()-> userService.addUser(userModel));
+        // then
+        BDDMockito.verify(userRepository,Mockito.never()).save(userEntity);
+    }
+
+}
